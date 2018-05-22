c89423ae20be9082060ac30ae47b585094b4cdd4
dhanyatha-harish <38994415+dhanyatha-harish@users.noreply.github.com>
2018-05-22 14:17:42 -0400

    Add files via upload

--- ttc/customer_service.py
+++ ttc/customer_service.py
@@ -1,46 +1,128 @@
+"""
+Py script name : customer_service.py
+Author : Dhanyatha Mylanahalli Harish
+Modules used : urllib, BeautifulSoup, pyycopg2, sqlalchemy
+Purpose : Fetch the Customer Service Report from TTC website and add these data daily into DataBAse
+"""
 import urllib.request
 from bs4 import BeautifulSoup
 import pandas as pd
-import collections as co
+import psycopg2
+import sys
+from sqlalchemy import create_engine
 
 # URL to the required web page
 ttc = "http://www.ttc.ca/Customer_Service/Daily_Customer_Service_Report/index.jsp"
 
-# Query the website and return the HTML to the variable page
-page = urllib.request.urlopen(ttc)
+# Query the website and return the HTML to the variable 'page'
+try:
+    page = urllib.request.urlopen(ttc)
+except Exception as page_error:
+    print("An exception occurred while trying to open a web page and the exception is ", page_error)
 
 # Parse the HTML in the page variable and store it in BeautifulSoup format
 soup = BeautifulSoup(page, "html.parser")
 
-# Print the HTML file that is parsed
-# print(soup.prettify())
+# Pick the table that has to be parsed using the class name of the table
+try:
+    right_table = soup.find('table', class_='ttc-customer-service-table')
+except Exception as table_error:
+    print("An exception occurred while finding the right table to fetch using class name and the exception is ", table_error)
 
-all_tables = soup.find_all('table')
+# Create and update a list called 'header' to save the column names of the table
+try:
+    header = []
+    for head in right_table.findAll("thead"):
+        cells = head.findAll('th')
+        for c_head in cells:
+            header.append(c_head.find(text=True))
+    del header[0]
+except Exception as header_error:
+    print("An exception occurred while fetching the column names and the exception is ", header_error)
 
-# for table in all_tables:
-#     print(table)
+# Create and update a list called 't_val' to save the column values of the table
+# by iterating through every row and every cell of the table
+try:
+    t_val = []
+    for rows in right_table.findAll('tbody'):
+        values = rows.findAll('td')
+        for c_vals in values:
+            if c_vals.find('img'):
+                result = c_vals.findAll(text=False)
+                result = str(result).split('"')[1]
+                if (result == "pass") or (result == "fail"):
+                    t_val.append(result)
+            else:
+                t_val.append(c_vals.findAll(text=True))
+except Exception as value_error:
+    print("An exception occurred while fetching all the values from a table and the exception is ", value_error)
 
-right_table = soup.find('table', class_='ttc-customer-service-table')
-# print(right_table)
+# Fetch the number of rows and columns in the table
+number_of_rows = len(right_table.findAll('tr'))-1
+number_of_columns = len(header)
 
-# Generate lists
-header=[]
-for head in right_table.findAll("thead"):
-    cells = head.findAll('th')
-    for i in cells:
-        header.append(i.find(text=True))
+# Sort each row values and put them in a tuple so as to put them in a DataFrame
+try:
+    row_vals = []
+    start = 0
+    end = number_of_columns
+    for loop in range((number_of_rows)):
+        del t_val[(start+1)][1]
+        tup = tuple(t_val[start:end])
+        row_vals.append(tup)
+        start = end
+        end = end + number_of_columns
+except Exception as rows_error:
+    print("An exception occurred while sorting all the values of the table into rows and the exception is ", rows_error)
 
-t_val = []
-# for v in range(8):
-#     t_val.append([])
-for rows in right_table.findAll('tbody'):
-    values = rows.findAll('td')
-    for j in values:
-        t_val.append(j.find(text=True))
+# Save the table data in DataFrame 'df'
+try:
+    df = pd.DataFrame.from_records(row_vals, columns=header)
+except Exception as dataframe_error:
+    print("An exception occurred while creating a DataFrame and the exception is ", dataframe_error)
 
-print(t_val)
+# Connect to a Database name 'interview_test'
+try:
+    con = psycopg2.connect("dbname=interview_test host=localhost user=postgres password=Dhanyatha")
+except psycopg2.ProgrammingError:
+    pass
 
+# Create a cursor for the connection for data manipulation
+cur = con.cursor()
 
+# Create a table "TTC_CSR"
+try:
+    cur.execute('CREATE TABLE TTC_CSR ()')
+except psycopg2.ProgrammingError:
+    # If the table is already there, ignore this exception
+    pass
 
-# df = pd.DataFrame(columns=header)
-# print(df)
+# Commit the changes made to the table
+con.commit()
+
+# Create and engine to enable the DataFrame to be pushed to DataBase
+try:
+    url = 'postgresql://postgres:Dhanyatha@localhost:5432/interview_test'
+    engine = create_engine(url, pool_pre_ping=True)
+except Exception as engine_error:
+    print("An exception occurred while creating an engine and the exception is ", engine_error)
+
+# Push the table data into PostgreSQL Database
+try:
+    df.to_sql("TTC_CSR", con=engine, if_exists='append')
+except Exception as df_db_error:
+    print("An exception occurred while pushing the dataframe to database and the exception is ", df_db_error)
+
+# Read back the table from DataBase into DataFrame and save it in "check" object
+try:
+    check = pd.read_sql_table("TTC_CSR", con=engine)
+except Exception as db_df_error:
+    print("An exception occurred while retrieving the table from database to dataframe and the exception is ",
+          db_df_error)
+
+# Print the data from Database for verification
+# print(check)
+
+# Close the connection
+cur.close()
+con.close()
